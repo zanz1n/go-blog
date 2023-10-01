7fb8f9818f93255676e747d43cfee30adc1c2bab
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-10-01 20:13:46 -0300

    removing unused script

--- website/src/index.ts
+++ website/src/index.ts
@@ -4,14 +4,3 @@ import "./scripts/themes";
 import "./global.css";
 import "./index.css";
 import "./header.css";
-
-import { fetchData } from "./lib/main";
-
-const element = document.getElementById("data");
-
-const result = fetchData();
-console.log("Fetched data: " + result);
-
-if (element) {
-    element.innerText = result;
-}
--- website/src/lib/main.ts
+++
@@ -1,3 +0,0 @@
-export function fetchData() {
-    return "Hello World";
-}
