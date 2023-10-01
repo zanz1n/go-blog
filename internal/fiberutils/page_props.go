49dd8811cb624e619f657bc666373a12f448c242
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-10-01 20:24:33 -0300

    updating util function

--- internal/fiberutils/page_props.go
+++ internal/fiberutils/page_props.go
@@ -1,8 +1,6 @@
 package fiberutils
 
 import (
-	"strings"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/zanz1n/go-htmx/internal/pages"
 )
@@ -12,27 +10,5 @@ func CreateProps[T any](
 	c *fiber.Ctx,
 	data T,
 ) pages.PageProps[T] {
-	pageName := ""
-
-	routes := make([]pages.Route, len(pp.Routes))
-	for i, v := range pp.Routes {
-		isCurrent := strings.Contains(c.Path(), v.Href)
-		if isCurrent {
-			pageName = v.Name
-		}
-
-		routes[i] = pages.Route{
-			IsCurrent: isCurrent,
-			Name:      v.Name,
-			Href:      v.Href,
-		}
-	}
-
-	return pages.PageProps[T]{
-		AppName:  pp.AppName,
-		PageName: pageName,
-		Routes:   routes,
-		User:     nil,
-		Data:     data,
-	}
+	return pages.CreateProps(pp, c.Path(), nil, data)
 }
