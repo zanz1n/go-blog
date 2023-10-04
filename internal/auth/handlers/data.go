6a3d7868aca9a2836dfc5f29de51a1bb1114f597
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-10-03 23:55:00 -0300

    updating struct tags

--- internal/auth/handlers/data.go
+++ internal/auth/handlers/data.go
@@ -1,7 +1,6 @@
 package handlers
 
 type LoginIdenPayload struct {
-	Email    string `json:"email" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required"`
 }
-
